f397f94009a3e02407d332fc22e5d6f289233412
mattperkins <github@mattperkins.com>
2018-06-20 15:57:43 +0100

    state ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import * as React from 'react'
 import './App.css'
 import MyComp from './MyComp'
 
-interface IProps { name?: string; isGood: boolean; count: number }
+interface IProps { name?: string; isGood?: boolean; count: number }
 
 interface IState { count: number; }
 
 class MyClass extends React.Component<IProps, IState> 
 /* <= if no props use <{}, IState>. If no state just use <IProps> */ 
+
 {
   static defaultProps: Partial<IProps> = {
     name: 'Fred'
-  }
+ }
 
-   state={
+   state: IState = {
     count: this.props.count
   }
 
@@ -32,7 +33,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-      <MyClass /* name="Sandy" <= using default props above */ isGood={true} count={11}/>
+      <MyClass /* name="Sandy" <= using default props above */ count={11}/>
       <MyComp />
       </div>
     )
