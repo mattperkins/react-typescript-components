604aa9b73d31bf42b31badafe9894fefa71ee31c
mattperkins <github@mattperkins.com>
2018-06-20 15:50:41 +0100

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import './App.css'
+import MyComp from './MyComp'
 
+interface IProps { name?: string; isGood: boolean; count: number }
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <p className="App-intro">
-          Hello, World!
-        </p>
+interface IState { count: number; }
+
+class MyClass extends React.Component<IProps, IState> 
+/* <= if no props use <{}, IState>. If no state just use <IProps> */ 
+{
+  static defaultProps: Partial<IProps> = {
+    name: 'Fred'
+  }
+
+   state={
+    count: this.props.count
+  }
+
+  render() {
+    const { name, count } = this.props
+    return(
+      <div>
+        <h1>{name}</h1>
+        <h2>{count}</h2>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <MyClass /* name="Sandy" <= using default props above */ isGood={true} count={11}/>
+      <MyComp />
+      </div>
+    )
+  }
+}
+
+
---
+++ src/MyComp.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+// type Props = {
+//   name: 'typeGuard' | 'one or the other of these',
+//   userLoggedIn?: ['Yes', 'No']
+// }
+
+const MyComp = () => <p>hello</p>
+
+export default MyComp
